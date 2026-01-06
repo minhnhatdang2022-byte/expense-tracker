8a8882d9eb43da01c0a8cb60e7affc3ade08d0bf
minhnhatdang2022-byte <minhnhatdang2022@gmail.com>
2026-01-06 13:59:58 +0700

    Create firebase.js

---
+++ js/config/firebase.js
@@ -0,0 +1,22 @@
+// js/config/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBVmLtZleXhPgHnEopx4jw2BIhIicHgIE",
+  authDomain: "quan-ly-thu-chi-fdda5.firebaseapp.com",
+  databaseURL: "https://quan-ly-thu-chi-fdda5-default-rtdb.firebaseio.com",
+  projectId: "quan-ly-thu-chi-fdda5",
+  storageBucket: "quan-ly-thu-chi-fdda5.firebasestorage.app",
+  messagingSenderId: "1063214543952",
+  appId: "1:1063214543952:web:5b43eb2766ac0bc2f1cd55",
+  measurementId: "G-MHB24KE4PL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
